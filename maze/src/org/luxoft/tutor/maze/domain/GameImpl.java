fbadcda3fb46d717982a29423527aa2312f8b1ff
User <user@luxoft.com>
2012-02-28 14:57:58 +0700

    Step 3:   GRASP pattern Creator and Interface

---
+++ maze/src/org/luxoft/tutor/maze/domain/GameImpl.java
@@ -0,0 +1,15 @@
+package org.luxoft.tutor.maze.domain;
+
+import org.luxoft.tutor.maze.api.Game;
+
+/**
+ * @author Maxim Yunusov
+ * @version 1.0
+ * @since <pre>2/28/12</pre>
+ */
+public class GameImpl implements Game {
+
+    public Maze createMaze() {
+        return null;  //TODO
+    }
+}
--- maze/test/org/luxoft/tutor/maze/TestMaze.java
+++ maze/test/org/luxoft/tutor/maze/TestMaze.java
@@ -1,7 +1,9 @@
 package org.luxoft.tutor.maze;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.luxoft.tutor.maze.api.Game;
 import org.luxoft.tutor.maze.domain.Door;
 import org.luxoft.tutor.maze.domain.Maze;
 import org.luxoft.tutor.maze.domain.Room;
@@ -9,33 +11,42 @@ import org.luxoft.tutor.maze.domain.Wall;
 
 public class TestMaze {
 
+    private Game game;
+
+    @Before
+    public void setUp() {
+        game = new Game() {
+
+            public Maze createMaze() {
+                final Maze maze = new Maze();
+
+                final Room room1 = maze.makeRoom(1);
+                final Room room2 = maze.makeRoom(2);
+
+                final Door door = new Door(room1, room2);
+
+                room1.setSide(Room.NORTH, new Wall());
+                room1.setSide(Room.EAST, door);
+                room1.setSide(Room.SOUTH, new Wall());
+                room1.setSide(Room.WEST, new Wall());
+
+                room2.setSide(Room.NORTH, new Wall());
+                room2.setSide(Room.EAST, new Wall());
+                room2.setSide(Room.SOUTH, new Wall());
+                room2.setSide(Room.WEST, door);
+                return maze;
+            }
+        };
+    }
+
     @Test
     public void testMazeBuild() throws Exception {
-        final Maze maze = createMaze();
+        final Maze maze = game.createMaze();
         final Room r1 = maze.romNo(1);
         final Room r2 = maze.romNo(2);
         final Door d = (Door) r2.getSide(Room.WEST);
         Assert.assertEquals(r1, d.otherSideFrom(r2));
     }
 
-    private Maze createMaze() {
-        final Maze maze = new Maze();
-
-        final Room room1 = maze.makeRoom(1);
-        final Room room2 = maze.makeRoom(2);
-
-        final Door door = new Door(room1, room2);
-
-        room1.setSide(Room.NORTH, new Wall());
-        room1.setSide(Room.EAST, door);
-        room1.setSide(Room.SOUTH, new Wall());
-        room1.setSide(Room.WEST, new Wall());
-
-        room2.setSide(Room.NORTH, new Wall());
-        room2.setSide(Room.EAST, new Wall());
-        room2.setSide(Room.SOUTH, new Wall());
-        room2.setSide(Room.WEST, door);
-        return maze;
-    }
 
 }

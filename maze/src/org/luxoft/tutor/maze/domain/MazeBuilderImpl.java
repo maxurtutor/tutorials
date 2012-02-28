8220c82ff550fb38a9494e0fa91f35340bed645d
User <user@luxoft.com>
2012-02-28 18:30:37 +0700

    Step 7:   GOF pattern Abstract Factory

--- maze/src/org/luxoft/tutor/maze/domain/MazeBuilderImpl.java
+++ maze/src/org/luxoft/tutor/maze/domain/MazeBuilderImpl.java
@@ -1,14 +1,7 @@
 package org.luxoft.tutor.maze.domain;
 
-import org.luxoft.tutor.maze.api.Door;
-import org.luxoft.tutor.maze.api.Maze;
+import org.luxoft.tutor.maze.api.MapSiteFactory;
 import org.luxoft.tutor.maze.api.MazeBuilder;
-import org.luxoft.tutor.maze.api.Room;
-
-import static org.luxoft.tutor.maze.api.Room.EAST;
-import static org.luxoft.tutor.maze.api.Room.NORTH;
-import static org.luxoft.tutor.maze.api.Room.SOUTH;
-import static org.luxoft.tutor.maze.api.Room.WEST;
 
 /**
  * @author Maxim Yunusov
@@ -17,70 +10,14 @@ import static org.luxoft.tutor.maze.api.Room.WEST;
  */
 public class MazeBuilderImpl extends MazeBuilder {
 
-    private final Maze maze;
-
-    public MazeBuilderImpl() {
-        this.maze = new MazeImpl();
+    @Override
+    protected MapSiteFactory makeFactory() {
+        return MapSiteFactoryImpl.getInstance();
     }
 
     @Override
-    public MazeBuilder addRoom(int room1) {
-        getRoom(room1);
-        return this;
-    }
-
-    @Override
-    public MazeBuilder addRoom(int room1, int room2, int side) {
-        getRoom(room1).setSide(side, getRoom(room2));
-        getRoom(room2).setSide(oppositeSide(side), getRoom(room1));
-        return this;
-    }
-
-    @Override
-    public MazeBuilder addDoor(final String id, int room1, int room2, int side) {
-        final Door door = DoorFactory.getInstance().make(id, getRoom(room1), getRoom(room2));
-        getRoom(room1).setSide(side, door);
-        getRoom(room2).setSide(oppositeSide(side), door);
-        return this;
-    }
-
-    @Override
-    public Maze build() {
-        return this.maze;
-    }
-
-    private int oppositeSide(int side) {
-        switch (side) {
-            case EAST:
-                return WEST;
-            case NORTH:
-                return SOUTH;
-            case SOUTH:
-                return NORTH;
-            case WEST:
-                return EAST;
-            default:
-                assert false : "Unknown side";
-                return -1;
-        }
-    }
-
-    private Room getRoom(int room) {
-        final Room result = this.maze.romNo(room);
-        return (result == null) ? makeRoom(room) : result;
-    }
-
-    private Room makeRoom(int room) {
-        final Room result = this.maze.makeRoom(room);
-        result.setSide(EAST, makeWall());
-        result.setSide(NORTH, makeWall());
-        result.setSide(SOUTH, makeWall());
-        result.setSide(WEST, makeWall());
-        return result;
-    }
-
-    private WallImpl makeWall() {
-        return new WallImpl();
+    protected MazeImpl makeMaze() {
+        return new MazeImpl();
     }
 
 }

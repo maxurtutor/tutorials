3273f4eaefe60bf6c76775b459803b6db08816d9
User <user@luxoft.com>
2012-03-01 13:09:35 +0700

    Step 10:  GoF pattern Adapter

---
+++ maze/src/org/luxoft/tutor/maze/graphics/GraphicsEngineAdapter.java
@@ -0,0 +1,19 @@
+package org.luxoft.tutor.maze.graphics;
+
+import org.luxoft.tutor.maze.api.GraphicsEngine;
+import org.luxoft.tutor.maze.api.Rectangle;
+
+/**
+ * @author Maxim Yunusov
+ * @version 1.0
+ * @since <pre>3/1/12</pre>
+ */
+public class GraphicsEngineAdapter
+        // extends SomeGraphicsEngine TODO
+        implements GraphicsEngine {
+
+    public void drawImage(final Rectangle rect, final String s) {
+        //Todo  someGraphicsEngine.drawImage(...)
+    }
+
+}

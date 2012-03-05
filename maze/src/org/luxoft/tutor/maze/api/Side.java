5049e87daa20ec59791937fb18b89c9d5057412b
User <user@luxoft.com>
2012-03-06 00:01:40 +0700

    Step 15:  Refactoring

--- maze/src/org/luxoft/tutor/maze/api/Side.java
+++ maze/src/org/luxoft/tutor/maze/api/Side.java
@@ -2,30 +2,30 @@ package org.luxoft.tutor.maze.api;
 
 public enum Side {
 
-    NORTH(0),
-    SOUTH(1),
-    EAST(2),
-    WEST(3);
-
-    final private int id;
-
-    private Side(int id) {
-        this.id = id;
-    }
-
-    public int id() {
-        return this.id;
-    }
-
-    public Side oppositeSide() {
-        switch (this) {
-            case EAST: return WEST;
-            case NORTH: return SOUTH;
-            case SOUTH: return NORTH;
-            case WEST: return EAST;
-            default: assert false: "Unknown side";
-                return null;
+    NORTH {
+        @Override
+        public Side opposite() {
+            return SOUTH;
         }
-    }
+    },
+    SOUTH {
+        @Override
+        public Side opposite() {
+            return NORTH;
+        }
+    },
+    EAST {
+        @Override
+        public Side opposite() {
+            return WEST;
+        }
+    },
+    WEST {
+        @Override
+        public Side opposite() {
+            return EAST;
+        }
+    };
 
+    public abstract Side opposite();
 }
--- maze/src/org/luxoft/tutor/maze/domain/DoorImpl.java
+++ maze/src/org/luxoft/tutor/maze/domain/DoorImpl.java
@@ -1,17 +1,8 @@
 package org.luxoft.tutor.maze.domain;
 
 import org.luxoft.tutor.maze.api.Door;
-import org.luxoft.tutor.maze.api.Room;
 
 public class DoorImpl extends Door {
-
-    public DoorImpl(final Room room1, final Room room2) {
-		super(room1, room2);
-	}
-
-    public DoorImpl() {
-
-    }
 
     @Override
     public void enter() {

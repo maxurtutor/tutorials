f9268fe10e2bb7eec735fbea12c72acf55a00a79
User <user@luxoft.com>
2012-03-07 01:31:46 +0700

    MAZE3:  Patterns:  Low Coupling, High Cohesion, Factory Method

--- maze/src/org/luxoft/tutor/maze/MazeGame.java
+++ maze/src/org/luxoft/tutor/maze/MazeGame.java
@@ -6,19 +6,16 @@ import org.luxoft.tutor.maze.api.Maze;
 import org.luxoft.tutor.maze.api.Room;
 import org.luxoft.tutor.maze.impl.DoorImpl;
 import org.luxoft.tutor.maze.impl.MazeImpl;
-import org.luxoft.tutor.maze.impl.RoomImpl;
 import org.luxoft.tutor.maze.impl.WallImpl;
 
 public final class MazeGame {
 
-    private Maze maze;
-
     public final Maze createMaze() {
 
-        maze = makeMaze();
+        final Maze maze = makeMaze();
 
-        final Room r1 = makeRoom(1);
-        final Room r2 = makeRoom(2);
+        final Room r1 = maze.makeRoom(1);
+        final Room r2 = maze.makeRoom(2);
 
         final Door d = makeDoor(r1, r2);
 
@@ -47,9 +44,4 @@ public final class MazeGame {
         return new DoorImpl(r1, r2);
     }
 
-    private RoomImpl makeRoom(int number) {
-        final RoomImpl room = new RoomImpl(number);
-        maze.addRoom(room);
-        return room;
-    }
 }
--- maze/src/org/luxoft/tutor/maze/impl/MazeImpl.java
+++ maze/src/org/luxoft/tutor/maze/impl/MazeImpl.java
@@ -1,8 +1,13 @@
 package org.luxoft.tutor.maze.impl;
 
 import org.luxoft.tutor.maze.api.Maze;
+import org.luxoft.tutor.maze.api.Room;
 
 public final class MazeImpl extends Maze {
 
-
+    public Room makeRoom(final int number) {
+        final Room room = new RoomImpl(number);
+        addRoom(room);
+        return room;
+    }
 }

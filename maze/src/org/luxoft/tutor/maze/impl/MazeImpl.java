20cf87da9782325b27f5100f26b52242a1d8769d
User <user@luxoft.com>
2012-03-14 16:17:45 +0700

    MAZE4:  Patterns:  High Cohesion, Abstract Factory

--- maze/src/org/luxoft/tutor/maze/impl/MazeImpl.java
+++ maze/src/org/luxoft/tutor/maze/impl/MazeImpl.java
@@ -1,12 +1,15 @@
 package org.luxoft.tutor.maze.impl;
 
+import org.luxoft.tutor.maze.api.MapSiteFactory;
 import org.luxoft.tutor.maze.api.Maze;
 import org.luxoft.tutor.maze.api.Room;
 
 public final class MazeImpl extends Maze {
 
+    private final MapSiteFactory mapSiteFactory = new MapSiteFactoryImpl();
+
     public Room makeRoom(final int number) {
-        final Room room = new RoomImpl(number);
+        final Room room = mapSiteFactory.makeRoom(number);
         addRoom(room);
         return room;
     }

e71b847331cc15f84ad6ceb9677317cde7e7e310
Maxim Yunusov <maximyunusov@gmail.com>
2012-07-01 19:03:00 +0700

    MONO_0

---
+++ src/org/maxur/monopoly/domain/Game.java
@@ -0,0 +1,8 @@
+package org.maxur.monopoly.domain;
+
+/**
+ * @author Maxim Yunusov
+ * @version 1.0 01.07.12
+ */
+public class Game {
+}
---
+++ test/org/maxur/monopoly/domain/GameTest.java
@@ -0,0 +1,9 @@
+package org.maxur.monopoly.domain;
+
+/**
+ * @author Maxim Yunusov
+ * @version 1.0 01.07.12
+ */
+public class GameTest {
+
+}

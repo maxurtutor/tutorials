0ee4b2295188492a12fe540db725661e8e5ade82
Maxim Yunusov <maximyunusov@gmail.com>
2012-07-01 19:31:15 +0700

    MONO_1

--- src/org/maxur/monopoly/domain/Game.java
+++ src/org/maxur/monopoly/domain/Game.java
@@ -5,4 +5,40 @@ package org.maxur.monopoly.domain;
  * @version 1.0 01.07.12
  */
 public class Game {
+
+    private int playersNumber;
+
+    private Game(int playersNumber) {
+        this.playersNumber = playersNumber;
+    }
+
+    public int getPlayersNumber() {
+        return this.playersNumber;
+    }
+
+    public static GameBuilder builder() {
+        return new GameBuilder();
+    }
+
+    public void play() {
+        this.playersNumber = 1;
+    }
+
+    public static class GameBuilder {
+        private static final int MIN_PLAYER_NUMBER = 2;
+        private static final int MAX_PLAYER_NUMBER = 8;
+        private int playersNumber;
+
+        public GameBuilder setPlayersNumber(int playersNumber) {
+            if (MIN_PLAYER_NUMBER > playersNumber || MAX_PLAYER_NUMBER < playersNumber) {
+                throw new IllegalArgumentException("");
+            }
+            this.playersNumber = playersNumber;
+            return this;
+        }
+
+        public Game build() {
+            return new Game(playersNumber);
+        }
+    }
 }
--- test/org/maxur/monopoly/domain/GameTest.java
+++ test/org/maxur/monopoly/domain/GameTest.java
@@ -1,9 +1,51 @@
 package org.maxur.monopoly.domain;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * @author Maxim Yunusov
  * @version 1.0 01.07.12
  */
 public class GameTest {
+
+    @Test
+    public void testMinPlayersNumberIs2() throws Exception {
+        final Game game = Game.builder().setPlayersNumber(2).build();
+        int playersNumber = game.getPlayersNumber();
+        assertEquals(2, playersNumber);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMinPlayersNumberMoreThan1() throws Exception {
+        Game.builder().setPlayersNumber(1).build();
+    }
+
+    @Test
+    public void testMaxPlayersNumberIs8() throws Exception {
+        final Game game = Game.builder().setPlayersNumber(8).build();
+        int playersNumber = game.getPlayersNumber();
+        assertEquals(8, playersNumber);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMaxPlayersNumberLessThan9() throws Exception {
+        Game.builder().setPlayersNumber(9).build();
+    }
+
+    @Test
+    public void testGameMustBeTerminatedOnOnePlayer() throws Exception {
+        final Game game = Game.builder().setPlayersNumber(2).build();
+        game.play();
+        int playersNumber = game.getPlayersNumber();
+        assertEquals(1, playersNumber);
+    }
+
+    @Test
+    public void testWinnerIsPlayer() throws Exception {
+
+    }
+
 
 }

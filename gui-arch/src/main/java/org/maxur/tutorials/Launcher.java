f70c8fd1c653bc9595852b919521be97c4bbc1ed
maximyunusov <maximyunusov@gmail.com>
2012-10-10 17:59:38 +0700

    GUI_ARCH: Separated Presentation

---
+++ gui-arch/src/main/java/org/maxur/tutorials/Launcher.java
@@ -0,0 +1,19 @@
+package org.maxur.tutorials;
+
+import javax.swing.*;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        final PersonModelSimpleImpl model = new PersonModelSimpleImpl();
+        final PersonForm form = new PersonForm(model);
+        model.addObserver(form);
+
+        final JFrame frame = new JFrame("Registration Form");
+        frame.setContentPane(form.getMainPanel());
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+}
